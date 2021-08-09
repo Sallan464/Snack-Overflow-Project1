755f75afd22e2a2c3634c5404517cf835acc615f
cerise-at <cerise.athompson@gmail.com>
2021-08-09 11:12:35 +0100

    added posts

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const port = process.env.PORT   || 8080
 // use to connect to front end files once we merge the branches
 //app.use(express.static(__dirname));
 
+let posts = [
+    Post.newPost("https://realfood.tesco.com/media/images/RFO-1400x919-ChickenClubSandwich-0ee77c05-5a77-49ac-a3bd-4d45e3b4dca7-0-1400x919.jpg", "chicken triangle with dip"),
+    Post.newPost("https://img.taste.com.au/c33UcYVI/taste/2016/11/giant-club-sandwich-110755-1.jpeg", "big boi chicken bacon lettuce thing"),
+    Post.newPost("https://assets.bonappetit.com/photos/57aca69153e63daf11a4d915/5:4/w_3515,h_2812,c_limit/california-veggie-sandwich.jpg", "california veggie"),
+    Post.newPost("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-200511-seo-club-sandwich-h-14383-eb-1590780714.jpg", "ham triangle with crisps")
+];
+
 //routes
 app.get("/", (req, res) => {
     res.send("index");
@@ -20,9 +27,3 @@ app.get("/info", (req, res) => {
     res.send(posts)
 })
 
-let posts = [
-    Post.newPost("https://realfood.tesco.com/media/images/RFO-1400x919-ChickenClubSandwich-0ee77c05-5a77-49ac-a3bd-4d45e3b4dca7-0-1400x919.jpg", "chicken triangle with dip"),
-    Post.newPost("https://img.taste.com.au/c33UcYVI/taste/2016/11/giant-club-sandwich-110755-1.jpeg", "big boi chicken bacon lettuce thing"),
-    Post.newPost("https://assets.bonappetit.com/photos/57aca69153e63daf11a4d915/5:4/w_3515,h_2812,c_limit/california-veggie-sandwich.jpg", "california veggie"),
-    Post.newPost("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-200511-seo-club-sandwich-h-14383-eb-1590780714.jpg", "ham triangle with crisps")
-];
